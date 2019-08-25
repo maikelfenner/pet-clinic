d800416d34cdcccc3aaade11035523eec8d453c0
Maikel Fenner <maikel.jsf@gmail.com>
2019-08-24 21:50:22 -0300

    Implement Interfaces for Services. Closes #5.

---
+++ pet-clinic-data/src/main/java/br/com/maikelfenner/services/VetService.java
@@ -0,0 +1,14 @@
+package br.com.maikelfenner.services;
+
+import br.com.maikelfenner.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
