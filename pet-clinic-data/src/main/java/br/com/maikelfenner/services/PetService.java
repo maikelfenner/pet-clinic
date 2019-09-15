a76f46f57020c4157e8bf4d5c263905bf024ecb3
Maikel Fenner <maikel.jsf@gmail.com>
2019-09-14 21:44:55 -0300

    Refactoring to common crud interface. Closes #11.

--- pet-clinic-data/src/main/java/br/com/maikelfenner/services/PetService.java
+++ pet-clinic-data/src/main/java/br/com/maikelfenner/services/PetService.java
@@ -2,13 +2,7 @@ package br.com.maikelfenner.services;
 
 import br.com.maikelfenner.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
 
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
