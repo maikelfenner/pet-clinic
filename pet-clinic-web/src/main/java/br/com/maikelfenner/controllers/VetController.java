2a03fc9b9aa2a5da7e94a05789b40e197388451f
Maikel Fenner <maikel.jsf@gmail.com>
2019-09-20 17:57:32 -0300

    Added vets list to index page and changed controller class. Closes #15.

--- pet-clinic-web/src/main/java/br/com/maikelfenner/controllers/VetController.java
+++ pet-clinic-web/src/main/java/br/com/maikelfenner/controllers/VetController.java
@@ -1,14 +1,24 @@
 package br.com.maikelfenner.controllers;
 
+import br.com.maikelfenner.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
